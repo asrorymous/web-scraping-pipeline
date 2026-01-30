f510a3667c244dedae9372229c5eb0954ca718fb
Asrorymous <asrorymous@gmail.com>
2026-01-30 21:09:01 +0700

    feat: add smart sync logic to skip existing images

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -3,36 +3,50 @@
  * and delegating the download task to the utility module.
  */
 
+const path = require("path");
+const fs = require("fs");
 const { downloadFile } = require("../utils/downloader");
 
 async function runImageDownloader(dbClient) {
   console.log("--- Starting Image Downloading Process --- ");
 
   // Fetch product data that has an associated image url
+
   const res = await dbClient.query(
-    "SELECT name, image_url FROM products Where image_url IS NOT NULL",
+    "SELECT name, image_url FROM products WHERE image_url IS NOT NULL",
   );
 
   console.log(`Found ${res.rows.length} images to process.`);
 
   // iterate throught each record and trigger the download
+
   for (let i = 0; i < res.rows.length; i++) {
     const row = res.rows[i];
 
-    /* * Sanitize and format the URL.
-     * The website uses relative paths like "../../", so we remove them
-     * to form a valid absolute URL.
-     */
+    // Create a URL-friendly filename (slug)
+    const fileName = `${row.name.replace(/[^a-z0-9]/gi, "_").toLowerCase()}.jpg`;
+
+    // Construct the full path to the image file
+    const filePath = path.join(__dirname, "../../output/images", fileName);
+
+    // --- SMART SYNC LOGIC ---
+    // Check if the file already exists in our folder
+    if (fs.existsSync(filePath)) {
+      console.log(
+        `[${i + 1}/${res.rows.length}] Skipping: ${fileName} (Already exists)`,
+      );
+      continue; // Langsung lanjut ke baris berikutnya, nggak usah download
+    }
+
     const cleanUrl =
       "https://books.toscrape.com/" + row.image_url.replace(/(\.\.\/)+/g, "");
 
-    // creat a URL-friendly filename (slug) based on the product name
-    const fileName = `${row.name.replace(/[^a-z0-9]/gi, "_").toLowerCase()}.jpg`;
-
     console.log(`[${i + 1}/${res.rows.length}] Downloading: ${fileName}`);
 
+    // let do it
     await downloadFile(cleanUrl, fileName);
   }
+
   console.log("--- Image Download Process Completed ---");
 }
 
